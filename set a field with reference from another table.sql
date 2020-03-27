15acbfedfc7d1a142a3f65360cff6f6a87ba2a0a
Abdellah Benkorich <abdellah.benkorich@gmail.com>
2020-03-27 10:53:28 +0100

    Update set a field with reference from another table.sql

--- set a field with reference from another table.sql
+++ set a field with reference from another table.sql
@@ -1,4 +1,10 @@
 update table_1 t1 
 left JOIN table_2 t2 
 on t1.reference_column = t2.reference_column 
-set t1.column_to_be_update = t2.column from where get data 
+set t1.column_to_be_update = t2.source_column
+
+# using the like % pattern
+update table1 t1 
+left join table2 t2 
+on t2.reference_column LIKE CONCAT('%', t1.reference_column ,'%')
+set t1.column_to_be_update = t2.source_column
